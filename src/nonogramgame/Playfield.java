177fd570062cc1f4ab56b5219ebbf1556cb51ee2
eric <eric.ekstrom@edumentor.se>
2019-12-01 22:37:56 +0100

    Added visual confimation of mouse dragging to the playfield

--- src/nonogramgame/Playfield.java
+++ src/nonogramgame/Playfield.java
@@ -5,10 +5,11 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 import javax.swing.JPanel;
 
-public class Playfield extends JPanel implements MouseListener {
+public class Playfield extends JPanel implements MouseListener, MouseMotionListener {
 
     int nonogramWidth = 0;
     int nonogramHeight = 0;
@@ -19,6 +20,7 @@ public class Playfield extends JPanel implements MouseListener {
 
         setPreferredSize(new Dimension(windowWidth, windowHeight));
         addMouseListener(this);
+        addMouseMotionListener(this);
     }
 
     public int[][] getNonogram() {
@@ -50,6 +52,8 @@ public class Playfield extends JPanel implements MouseListener {
             }
         }
 
+        drawMouseDragged(g);
+
         //draws the grid
         for (int i = 0; i < Math.max(nonogramWidth, nonogramHeight); i++) {
             if (i % 5 == 0) {
@@ -57,7 +61,7 @@ public class Playfield extends JPanel implements MouseListener {
             } else {
                 g.setColor(Color.LIGHT_GRAY);
             }
-            
+
             g.drawLine((int) (i * NonogramFrame.squareSize), 0, (int) (i * NonogramFrame.squareSize), window.height);
             g.drawLine(0, (int) (i * NonogramFrame.squareSize), window.width, (int) (i * NonogramFrame.squareSize));
         }
@@ -74,37 +78,44 @@ public class Playfield extends JPanel implements MouseListener {
     int startX;
     int startY;
 
+    int movedX;
+    int movedY;
+    int mouseButtonPressed = -1;
+
     @Override
     public void mouseClicked(MouseEvent e) {
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
+        mouseButtonPressed = e.getButton();
+
         Dimension window = getPreferredSize();
-        
+
         startX = e.getX() * nonogramWidth / window.width;
         startY = e.getY() * nonogramHeight / window.height;
-        
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        mouseButtonPressed = -1;
         Dimension window = getPreferredSize();
         int endX = e.getX() * nonogramWidth / window.width;
         int endY = e.getY() * nonogramHeight / window.height;
-        
+
         if (endX < startX) {
             int temp = endX;
             endX = startX;
             startX = temp;
         }
-        
+
         if (endY < startY) {
             int temp = endY;
             endY = startY;
             startY = temp;
         }
-        
+
         int type = 0;
         switch (e.getButton()) {
             case MouseEvent.BUTTON1:
@@ -117,13 +128,13 @@ public class Playfield extends JPanel implements MouseListener {
                 type = 0;
                 break;
         }
-        
+
         for (int i = startX; i <= endX; i++) {
             for (int j = startY; j <= endY; j++) {
                 nonogram[i][j] = type;
             }
         }
-        
+
         repaint();
     }
 
@@ -133,5 +144,53 @@ public class Playfield extends JPanel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Dimension window = getPreferredSize();
+        movedX = e.getX() * nonogramWidth / window.width;
+        movedY = e.getY() * nonogramHeight / window.height;
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void drawMouseDragged(Graphics g) {
+        if (mouseButtonPressed != -1) {
+            int tempStartX = startX;
+            int tempStartY = startY;
+
+            if (movedX < tempStartX) {
+                int temp = movedX;
+                movedX = tempStartX;
+                tempStartX = temp;
+            }
+
+            if (movedY < tempStartY) {
+                int temp = movedY;
+                movedY = tempStartY;
+                tempStartY = temp;
+            }
+
+            switch (mouseButtonPressed) {
+                case MouseEvent.BUTTON1:
+                    g.setColor(Color.BLACK);
+                    break;
+                case MouseEvent.BUTTON2:
+                    g.setColor(Color.CYAN);
+                    break;
+                default:
+                    g.setColor(Color.WHITE);
+                    break;
+            }
+            for (int i = tempStartX; i <= movedX; i++) {
+                for (int j = tempStartY; j <= movedY; j++) {
+                    g.fillRect((int) NonogramFrame.squareSize * i, NonogramFrame.squareSize * j, NonogramFrame.squareSize, NonogramFrame.squareSize);
+                }
+            }
+        }
     }
 }
